feb318d79d8b4ffb52c506507acb5aaaaab803f6
Winki <163889519+Winki372@users.noreply.github.com>
2025-12-31 05:18:01 +0700

    Create trg_otomatis_mutasi_awal.sql

---
+++ Final Project/trg_otomatis_mutasi_awal.sql
@@ -0,0 +1,4 @@
+CREATE DEFINER=`root`@`localhost` TRIGGER `trg_otomatis_mutasi_awal` AFTER INSERT ON `buku` FOR EACH ROW BEGIN
+    INSERT INTO mutasi_buku (id_buku, jenis_mutasi, jumlah, keterangan)
+    VALUES (NEW.id_buku, 'Masuk', NEW.stok_total, 'Stok Awal (Auto Trigger)');
+END
