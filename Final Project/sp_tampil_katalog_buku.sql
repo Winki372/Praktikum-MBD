1dc618eaf1a1e631b3f91adbfda96451927db121
Winki <163889519+Winki372@users.noreply.github.com>
2025-12-31 05:34:07 +0700

    Create sp_tampil_katalog_buku.sql

---
+++ Final Project/sp_tampil_katalog_buku.sql
@@ -0,0 +1,18 @@
+create procedute `sp_tampil_katalog_buku`(
+  in p_keyword
+)
+begin
+  select
+    buku.id_buku,
+    buku.judul_buku,
+    buku.pengarang,
+    buku.stok_tersedia
+
+    group concat(genre.nama_genre separator ', ') as kategori_genre
+from buku
+left join relasi_buku_genre on buku.id_buku = relasi_buku_genre.id_buku
+left join genre on relasi_buku_genre.id_genre = genre.id_genre
+
+where buku.judul_buku like concat('%', p_keyword, '%')
+group by buku.id_buku;
+end
