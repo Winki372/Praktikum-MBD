9014eaa98072b4bf735e9260cad88fd95ffa3aeb
Winki <163889519+Winki372@users.noreply.github.com>
2025-12-31 05:14:57 +0700

    Create f_estimasi_denda.sql

---
+++ Final Project/f_estimasi_denda.sql
@@ -0,0 +1,18 @@
+
+CREATE DEFINER=`root`@`localhost` FUNCTION `f_estimasi_denda`( 
+    p_tgl_jatuh_tempo DATE,  
+    p_tgl_kembali DATE 
+) RETURNS decimal(10,2)
+    DETERMINISTIC
+BEGIN 
+    DECLARE v_selisih_hari INT; 
+    DECLARE v_tarif_per_hari DECIMAL(10, 2) DEFAULT 2000; 
+    DECLARE v_total_denda DECIMAL(10, 2); 
+    SET v_selisih_hari = DATEDIFF(p_tgl_kembali, p_tgl_jatuh_tempo); 
+    IF v_selisih_hari > 0 THEN 
+        SET v_total_denda = v_selisih_hari * v_tarif_per_hari; 
+    ELSE 
+        SET v_total_denda = 0; 
+    END IF; 
+    RETURN v_total_denda; 
+END
